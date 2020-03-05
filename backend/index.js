bc778a24536860ce9a9c1bd61c999a63acd04715
Felix Gerberding <gerberding.felix@gmail.com>
2020-03-05 17:51:34 +0100

    removed unnecessary dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-const sketch = require("sketchrnn");
 const https = require('https');
 var http = require('http')
 var io = require('socket.io').listen(http);
@@ -217,76 +216,4 @@ io.on('connection', function(socket){
 
 });
 
-app.listen(3000);
-
-// --- Model Setup --- //
-
-function sleep(time, callback) {
-    var stop = new Date().getTime();
-    while(new Date().getTime() < stop + time) {
-        ;
-    }
-    callback();
-}
-
-function outputDrawStream(socket){
-
-  const modelUrl = "https://storage.googleapis.com/quickdraw-models/sketchRNN/large_models/bus.gen.json";
-
-  const request = https.get(modelUrl,
-      res => {
-          res.setEncoding("utf8");
-          let body = ""
-          res.on("data", data => {
-              body += data;
-          });
-          res.on('end', () => makeModel(body, socket));
-      }
-  );
-
-  function makeModel(_json, socket){
-    var hrstart = process.hrtime()
-    var modelData = JSON.parse(_json);
-    var model = new sketch.SketchRNN(modelData);
-    hrend = process.hrtime(hrstart)
-    console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
-
-    let model_data = model.zero_state();
-    let result = [];
-
-    let stopLoop = 0;
-    let strokes = [];
-
-    if (Array.isArray(strokes) && strokes.length) {
-     model.updateStrokes(strokes, this.rnnState);
-   }
-
-   while (stopLoop != 1) {
-     //sleep(1000, function() {
-
-        const pdf = model.get_pdf(model_data, 0.25);
-
-        let things = model.sample(pdf);
-        const result = {
-          dx: things[0],
-          dy: things[1],
-        };
-        if (things[2] === 1) {
-          result.pen = 'down';
-        } else if (things[3] === 1) {
-          result.pen = 'up';
-        } else if (things[4] === 1) {
-          result.pen = 'end';
-          stopLoop = 1;
-        }
-        modelState = model.update(things, model_data);
-        socket.emit('draw', result);
-        console.log(result)
-      //})
-
-    }
-
-
-  }
-
-}
+app.listen(process.env.PORT || 3000);
