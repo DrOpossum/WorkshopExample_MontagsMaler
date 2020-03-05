2cddfe1ab1ca557f3808e5f9da4e39c51fbee420
Felix Gerberding <gerberding.felix@gmail.com>
2020-03-05 18:16:28 +0100

    file restructuring

--- index.js
+++ index.js
@@ -123,10 +123,57 @@ const models = [
 
 index = fs.readFileSync(__dirname + '/play.html');
 
-var app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
-});
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.zip': 'application/zip',
+  '.doc': 'application/msword',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.ttf': 'application/x-font-ttf',
+};
+
+var app = http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+  const parsedUrl = url.parse(req.url);
+
+  const sanitizePath = path.normalize(parsedUrl.pathname).replace(/^(\.\.[\/\\])+/, '');
+  let pathname = path.join(__dirname, sanitizePath);
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += '/play.html';
+    }
+
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        const ext = path.parse(pathname).ext;
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+
+
+})
 
 var io = require('socket.io').listen(app);
 
@@ -217,4 +264,5 @@ io.on('connection', function(socket){
 
 });
 
+app.use(express.static('assets'));
 app.listen(process.env.PORT || 3000);
